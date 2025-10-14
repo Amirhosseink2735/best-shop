6816e937050e84b32630ab58fa9e27eedac2c28f
unknown <amirhosseinkazemi534@gmail.com>
2025-10-14 23:53:53 +0330

    pause order app

--- shop/apps/orders/urls.py
+++ shop/apps/orders/urls.py
@@ -4,5 +4,7 @@ import apps.orders.views as vv
 app_name="ord"
 
 urlpatterns = [
-
+    path("add_shopcart/",vv.add_to_shopcart,name="add_shopcart"),
+    path("show_shopcart/",vv.Show_Shopcart.as_view(),name="show_shopcart"),
+    
 ]
--- shop/apps/orders/views.py
+++ shop/apps/orders/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from django.views import View
+from django.db.models import Q
+from .shop_cart import shopcart
+from apps.products.models import Product
 
-# Create your views here.
+#----------------------------------------------------------------
+#کلاس برای نمایش سبد خرید 
+class Show_Shopcart(View):
+    def get(self,request):
+        shop_cart=shopcart(request)
+        total_price=shop_cart.clc_total_price()
+        print(total_price)
+        
+        
+        context={
+            "shop_carts":shop_cart,
+            
+        }
+        
+        
+        return render(request,"orders/show_shopcart.html",context)
+
+
+
+
+
+
+
+#----------------------------------------------------------------
+#اضافه کردن به سبد خرید 
+def add_to_shopcart(request):
+    shop_cart=shopcart(request)
+    product_id=request.GET.get("product_id")
+    product=get_object_or_404(Product,id=product_id)
+    qty=request.GET.get("qty")
+    shop_cart.add_to_shop_cart(product,qty)
+    return HttpResponse("محصول به سبد خرید اضافه شد !!!")
+    
+
+#----------------------------------------------------------------
+
+
+
+
+
+
+
+
