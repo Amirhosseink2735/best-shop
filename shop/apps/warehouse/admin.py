6bb7bf12a6b9734535af5ed32587c5f4617aea50
unknown <amirhosseinkazemi534@gmail.com>
2025-10-19 00:01:43 +0330

    end of admin panel

--- shop/apps/warehouse/admin.py
+++ shop/apps/warehouse/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import WarehousesType,Warehouses
+#-------------------------------------------------------------------
+@admin.register(WarehousesType)
+class WarehousesTyoeAdmin(admin.ModelAdmin):
+    list_display=("id","warehouses_type_title")
+#-------------------------------------------------------------------
+    
+@admin.register(Warehouses)
+class WarehousesAdmin(admin.ModelAdmin):
+    list_display=("product","price","qty","warehouses_type","registered_data")
+#-------------------------------------------------------------------
