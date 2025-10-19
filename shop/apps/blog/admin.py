788618a0abaa3036f91ed3c6f93a1b78793c1515
unknown <amirhosseinkazemi534@gmail.com>
2025-10-19 23:14:46 +0330

    end of admin panel

--- shop/apps/blog/admin.py
+++ shop/apps/blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Blog
 
-# Register your models here.
+#----------------------------------------------------------
+def active_products(modeladmin,request,queryset):
+    res=queryset.update(is_active=True)
+    message=f"تعداد{res}فعال شد."
+    modeladmin.message_user(request,message)
+    
+def de_active_products(modeladmin,request,queryset):
+    res=queryset.update(is_active=False)
+    message=f"تعداد{res}مقاله غیر فعال شد "
+    modeladmin.message_user(request,message)
+
+@admin.register(Blog)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=("blog_name","blog_name","is_active","register_date")
+    list_filter=("is_active",)
+    list_editable=("is_active",)
+    ordering=("is_active","register_date")
+    actions=[active_products,de_active_products]
+    
+    
+    
+    
+    
