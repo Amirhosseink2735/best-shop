435f98739d4ed20e1814c2ea7cf3a565738a84c8
unknown <amirhosseinkazemi534@gmail.com>
2025-10-20 00:57:50 +0330

    pause

--- shop/apps/blog/urls.py
+++ shop/apps/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("readable/",vv.Readable,name="readable"),
     path("blog_details/<slug:slug>",vv.BlogDetails.as_view(),name="blog_details"),
     path("related_blogs/<slug:slug>",vv.related_blogs,name="related_blogs"),
+    path("show_blogs/",vv.Show_blogs.as_view(),name="show_blogs"),
 
 ]
--- shop/apps/blog/views.py
+++ shop/apps/blog/views.py
@@ -30,7 +30,16 @@ def related_blogs(request,slug):
     r_blogs=Blog.objects.filter(Q(is_active=True)&Q(blog_group__in=blog_groups)).exclude(id=blog.id).distinct()[:3]
     return render(request,"blog/related_blogs.html",{"r_blogs":r_blogs})
 
-    
+#-----------------------------------------------------
+
+#نمایش تمای مقالات 
+class Show_blogs(View):
+    def get(self,request):
+        blogs=Blog.objects.filter(Q(is_active=True)).order_by("-published_date")[:5]
+        return render(request,"blog/show_blogs.html",{"blogs":blogs})
+
+
+
 
 
 
